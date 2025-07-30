b0fc41becef0ad8f60d81176aa461323a459c40c
Thong Yuan Yu, Sarah <86669600+sarah-yy@users.noreply.github.com>
2025-07-30 08:14:49 +0000

    Setup project workspace and dependencies

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+export default function Page() {
+  return (
+    <Fragment>
+      <h1>This is a heading</h1>
+      <p>This is a paragraph</p>
+    </Fragment>
+  );
+}
